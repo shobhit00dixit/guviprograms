4bed6f1584da114561145bc1fe1aadf794228ad3
shobhit00dixit <shobhit00dixit@gmail.com>
2018-10-11 00:09:22 +0530

    My First Guvi Program

---
+++ guvi_s1_q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    if(a<0){
+        cout<<"Negative";
+    }
+    else if(a>0){
+        cout<<"Positive";
+    }
+    else{
+        cout<<"Zero";
+    }
+}
